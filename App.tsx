585ddaafe3b70a625cf6e4ab1931d88ce35ab6ee
Elkanah <elkee2003@yahoo.com>
2024-02-20 11:10:11 +0100

    Upgraded workig version RN without amplify

--- App.tsx
+++ App.tsx
@@ -8,24 +8,7 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {StatusBar} from 'react-native';
 
-import { withAuthenticator } from '@aws-amplify/ui-react-native';
-
 import RootNavigator from './src/Navigation/Root';
-
-// import { Amplify } from 'aws-amplify';
-// import awsconfig from './src/aws-exports';
-
-// Amplify.configure({
-//   ...awsconfig,
-//   Analytics: {
-//     disabled: true,
-//   },
-// });
-
-
-// import { Amplify } from 'aws-amplify';
-// import amplifyconfig from './src/amplifyconfiguration.json';
-// Amplify.configure(amplifyconfig);
 
 
 const App = ()=>{
--- index.js
+++ index.js
@@ -4,12 +4,12 @@
 import 'react-native-url-polyfill/auto';
 import 'react-native-get-random-values';
 
-import { Amplify } from 'aws-amplify';
-import amplifyconfig from './src/amplifyconfiguration.json';
-Amplify.configure(amplifyconfig);
-
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+// import { Amplify } from 'aws-amplify';
+// import amplifyconfig from './src/amplifyconfiguration.json';
+// Amplify.configure(amplifyconfig);
 
 AppRegistry.registerComponent(appName, () => App);
